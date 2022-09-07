3bb946feda142b4fce49b9d0a4d669a2ca19e38f
adlihardiyanto <adlihardiyanto@gmail.com>
2022-09-07 20:45:55 +0700

    revised

--- index.js
+++ index.js
@@ -5,27 +5,47 @@ const port = process.env.PORT||4000;
 const ejs = require('ejs')
 const models = require('./models')
 const {Op} = require('sequelize')
+const fs = require('fs')
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 
+app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+
+const readJson = fs.readFileSync('user.json');
+const players = JSON.parse(readJson);
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/users', (req, res) => {
+  res.send({username:players[0].username, password:'******'});
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+
+app.post('/login', (req, res) => {
+  const {username, password} = req.body;
+
+  if (username == players[0].username && password == players[0].password) {
+    res.redirect('/index-game');
+  } else if(username == players[1].username && password == players[1].password ) { 
+    res.redirect('/dashboard');
+  } 
+});
+
 app.get('/dashboard', async (req, res) => {
-    // const searchUser = req.query.search_user;
-    // let user;
-    // if (!searchUser) {
-    //     users = await models.UserGame.findAll()
-    // } else {
-    //    users = await models. UserGame.findAll({
-    //     where: {
-    //         username: {
-    //             [Op.iLike]: `%${searchUser}%`
-    //         }
-    //     }
-    //    })
-    // }
-    const users = await models.UserGame.findAll()
+    const users = await models.UserGame.findAll({
+        include: models.UserGameBio
+    })
     res.render('dashboard', {
-        users: users
+        users: users,
+        
     });
 })
 
@@ -59,7 +79,7 @@ app.post('/add-biodata/:id', async (req, res) => {
     const {id} = req.params;
     const {UserGameId, dob, pob, city, gender } = req.body;
     await models.UserGameBio.create({
-      UserGameId: UserGameId,
+      UserGameId: id,
       dob: dob,
       pob: pob,
       city: city,
@@ -99,8 +119,14 @@ app.get('/edit-user/:id', async (req, res) => {
             id:id
         }
     })
+    const biodata = await models.UserGameBio.findOne({
+        where: {
+            UserGameId : id
+        }
+    })
     res.render('edit-user', {
-        user: user
+        user: user,
+        biodata: biodata
     })
 })
 
@@ -111,12 +137,23 @@ app.post('/update/:id', async (req,res) => {
             id:id
         }
     })
+    const biodata = await models.UserGameBio.findOne({
+        where: {
+            UserGameId : id
+        }
+    })
     await user.update(req.body);
+    await biodata.update(req.body);
     res.redirect('/dashboard');
 })
+
+app.get('/index-game', (req, res) => {
+  res.render('index-game');
+});
 
 models.sequelize.authenticate().then(() => {
     app.listen(port, () => {
         console.log(`Server connected at http://localhost:${port}`)
     })
 }).catch(console.log)
+
